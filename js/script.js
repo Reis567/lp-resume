31c8315b384b8c5f7f44242b51f65d1dbffed70d
Reis567 <matheusdosreis9@gmail.com>
2023-10-04 16:45:33 -0300

    mobile animation to go to one section

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ const hamburguer = document.querySelector('.hamburguer');
 
 const nav = document.querySelector('.nav');
 
+const navLinks = document.querySelectorAll('.nav-list a');
+
 hamburguer.addEventListener("click", ()=> 
     nav.classList.toggle("active"));
+
+
+navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active"); 
+        });
+    });
